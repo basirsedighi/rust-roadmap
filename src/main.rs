e029e17bfb6c4deafb69847622b1a1800d6c4b8b
based <basir.sedighi@nris.no>
2024-04-14 18:30:20 +0200

    project structure init

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
 fn main() {
-    let my_integer:i8 = 5;  
-    print!("The value of my_integer is: {}", my_integer);
-    println!("my_integer is a signed 8-bit integer");
+    println!("Hello, world!");
 }
